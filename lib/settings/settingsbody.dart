a945c7c40e21b61aa3e354d00379fb3aa74b5557
Sowmya Thottambeti <sowmya6598@gmail.com>
2018-07-28 12:09:40 +0200

    settings

--- lib/settings/settingsbody.dart
+++ lib/settings/settingsbody.dart
@@ -7,10 +7,12 @@ class SettingsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
     Widget header = new Container(
       child: Container(
         color: Colors.green,
-        margin: new EdgeInsets.only(bottom: 100.0),
+        margin: new EdgeInsets.only(bottom: 20.0),
       ),
     );
 
@@ -35,16 +37,27 @@ class SettingsBody extends StatelessWidget {
                   image: new AssetImage('assets/picture.png'),
                   height: 40.0,
                   width: 50.0),
+              new Container(
+                child: SizedBox(width: 10.0),
+              ),
               new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   new Text("Update Email Address",
                       style: TextStyle(
                           color: Color(0xFFE08284),
-                          fontWeight: FontWeight.w600)),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15.0)),
+                  new Container(
+                    child: SizedBox(height: 5.0),
+                  ),
                   new Text("sowmya6598@gmail.com",
                       style: TextStyle(
                           color: Color(0xFFF1BFB9),
-                          fontWeight: FontWeight.w400)),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13.0)),
                 ],
               ),
               new IconButton(
@@ -78,16 +91,27 @@ class SettingsBody extends StatelessWidget {
                   image: new AssetImage('assets/picture.png'),
                   height: 40.0,
                   width: 50.0),
+              new Container(
+                child: SizedBox(width: 10.0),
+              ),
               new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   new Text("Change Password",
                       style: TextStyle(
                           color: Color(0xFFE08284),
-                          fontWeight: FontWeight.w600)),
-                  new Text("last changed",
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15.0)),
+                  new Container(
+                    child: SizedBox(height: 5.0),
+                  ),
+                  new Text("last changed 13 days ago",
                       style: TextStyle(
                           color: Color(0xFFF1BFB9),
-                          fontWeight: FontWeight.w400)),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13.0)),
                 ],
               ),
               new IconButton(
@@ -116,7 +140,9 @@ class SettingsBody extends StatelessWidget {
 //          },
           child: Text("Log out",
               style: TextStyle(
-                  color: Color(0xFFE08284), fontWeight: FontWeight.w600)),
+                  color: Color(0xFFE08284),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15.0)),
         ),
       ),
     );
@@ -143,16 +169,27 @@ class SettingsBody extends StatelessWidget {
                   image: new AssetImage('assets/picture.png'),
                   height: 40.0,
                   width: 50.0),
+              new Container(
+                child: SizedBox(width: 10.0),
+              ),
               new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   new Text("About Us",
                       style: TextStyle(
                           color: Color(0xFFE08284),
-                          fontWeight: FontWeight.w600)),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15.0)),
+                  new Container(
+                    child: SizedBox(height: 5.0),
+                  ),
                   new Text("who are we",
                       style: TextStyle(
                           color: Color(0xFFF1BFB9),
-                          fontWeight: FontWeight.w400)),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13.0)),
                 ],
               ),
               new IconButton(
@@ -187,19 +224,31 @@ class SettingsBody extends StatelessWidget {
                   image: new AssetImage('assets/picture.png'),
                   height: 40.0,
                   width: 50.0),
+              new Container(
+                child: SizedBox(width: 10.0),
+              ),
               new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   new Text("Contact Us",
                       style: TextStyle(
                           color: Color(0xFFE08284),
-                          fontWeight: FontWeight.w600)),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15.0)),
+                  new Container(
+                    child: SizedBox(height: 5.0),
+                  ),
                   new Text("We want to hear from you!",
                       style: TextStyle(
                           color: Color(0xFFF1BFB9),
-                          fontWeight: FontWeight.w400)),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13.0)),
                 ],
               ),
               new IconButton(
+                  alignment: Alignment.centerRight,
                   icon: new Icon(Icons.navigate_next),
                   onPressed: null,
                   color: Color(0xFFE08284)),
@@ -218,9 +267,26 @@ class SettingsBody extends StatelessWidget {
             scrollDirection: Axis.vertical,
             children: <Widget>[
               header,
+              new Text(
+                "ACCOUNT",
+                style: TextStyle(
+                    letterSpacing: 1.0,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 15.0),
+              ),
               emailButton,
               passButton,
               logout,
+              SizedBox(height: 25.0),
+              new Text(
+                "OTHER",
+                style: TextStyle(
+                    letterSpacing: 1.0,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 15.0),
+              ),
               aboutUs,
               contactUs
             ],
